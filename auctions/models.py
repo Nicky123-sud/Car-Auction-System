67b4aa940470cbfe0538a7034f64e2ddd130f3b5
Nicky123-sud <nicksononsombi515@gmail.com>
2025-03-13 09:30:39 +0300

    Refined Seller Dashboard UI and fixed layout issues

--- auctions/models.py
+++ auctions/models.py
@@ -186,16 +186,17 @@ class UserProfile(models.Model):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     
     def __str__(self):
-        return self.user.username
+        return f"Inspection Report for {self.auction.vehicle.make} {self.auction.vehicle.model}"
 
 
-class Payment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    transaction_id = models.CharField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=15)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=50, choices=[("Success", "Success"), ("Failed", "Failed")], default="Pending")
-    timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Payment for {self.user.username} - {self.transaction_id}"
+# class Payment(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     transaction_id = models.CharField(max_length=100, unique=True)
+#     phone_number = models.CharField(max_length=15)
+#     amount = models.DecimalField(max_digits=10, decimal_places=2)
+#     status = models.CharField(max_length=50, choices=[("Success", "Success"), ("Failed", "Failed")], default="Pending")
+#     timestamp = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"Payment for {self.user.username} - {self.transaction_id}"
